67721bdab67faed31d50be26cb4c377ad41698c1
Ar4sCode <ar4sazizi@gmail.com>
2025-02-21 01:26:25 +0330

    Add function to flip each die and track each die with its own state

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,49 @@
-import { useState } from 'react'
-import './App.css'
-import Die from './components/Die'
-import RollButton from './RollButton'
+import { useState } from 'react';
+import './App.css';
+import Die from './components/Die';
+import RollButton from './RollButton';
+import { nanoid } from 'nanoid';
 
 function App() {
-  const [diceNumbers, setDiceNumbers] = useState(generateAllNewDice())
-/*
-  const generateRandomInt = () => {
-    return Math.floor(Math.random() * 7 + 1)
+  const [diceNumbers, setDiceNumbers] = useState(generateAllNewDice());
+
+  function generateAllNewDice() {
+    let newDices = [];
+    for (let index = 1; index < 11; index++) {
+      newDices.push({
+        values: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid(),
+      });
+    }
+    return newDices;
   }
 
-  let diceArray = []
-  for(let index = 1; index < 11; index++) {
-    diceArray.push(generateRandomInt())
-  }
-*/
-  function generateAllNewDice()  {
-    let newDices = []
-    for(let index= 1; index < 11; index++) {
-      // we can use Math.ceil() => we don't need add 1 to our random number
-      newDices.push(Math.ceil(Math.random() * 6))
-    }
-    return newDices
-  }
+  const holdDice = (id) => {
+    setDiceNumbers((prevDice) =>
+      prevDice.map((die) =>
+        die.id === id ? { ...die, isHeld: !die.isHeld } : die
+      )
+    );
+  };
 
   return (
     <>
-      <main className='bg-white rounded-lg h-full flex flex-col justify-evenly items-center'>
-        <div className='grid grid-cols-5 gap-5'>
-          {diceNumbers.map((die, index) => {
-            return(<Die key={index} value={die} />)
-          })}
+      <main className="bg-white rounded-lg h-full flex flex-col justify-evenly items-center">
+        <div className="grid grid-cols-5 gap-5">
+          {diceNumbers.map((die) => (
+            <Die
+              onClick={() => holdDice(die.id)}
+              isHeld={die.isHeld} // Now each die tracks its own state
+              key={die.id}
+              value={die.values}
+            />
+          ))}
         </div>
-        <RollButton onClick={() => setDiceNumbers(generateAllNewDice)} />
+        <RollButton onClick={() => setDiceNumbers(generateAllNewDice())} />
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,6 +1,10 @@
-const Die = ({value}) => {
+// eslint-disable-next-line react/prop-types
+const Die = ({value, isHeld, onClick}) => {
+
   return (
-    <button className="h-4 w-4 p-5 border-1 border-gray-100 rounded-lg shadow-md flex justify-center items-center font-bold cursor-pointer text-2xl">
+    <button onClick={onClick} className={isHeld ? "h-4 w-4 p-5 bg-green-400 border-1 border-gray-100 rounded-lg shadow-md flex justify-center items-center font-bold cursor-pointer text-2xl duration-100 hover:outline-3 outline-gray-400" : 
+      "h-4 w-4 p-5 border-1  border-gray-100 rounded-lg shadow-md flex justify-center items-center font-bold cursor-pointer text-2xl duration-100 hover:outline-3 outline-gray-400"
+    }>
       {value}
     </button>
   )
